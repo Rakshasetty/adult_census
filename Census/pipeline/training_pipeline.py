c6768368ecdfee57d70eff99b006471bc8205d01
Rakshasetty <rakshasetty004@gmail.com>
2023-02-07 14:17:29 +0530

    Complete validation

--- Census/pipeline/training_pipeline.py
+++ Census/pipeline/training_pipeline.py
@@ -4,6 +4,7 @@ from Census.utils import get_collection_as_dataframe
 import sys,os
 from Census.entity import config_entity
 from Census.components.data_ingestion import DataIngestion
+from Census.components.data_validation import DataValidation
 
 def start_training_pipeline():
     try:
@@ -15,5 +16,16 @@ def start_training_pipeline():
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
 
+    
+ #data validation
+        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation = DataValidation(data_validation_config=data_validation_config,
+                        data_ingestion_artifact=data_ingestion_artifact)
+
+        data_validation_artifact = data_validation.initiate_data_validation()
+
     except Exception as e:
         raise SensorException(e, sys)
+
+
+ 
