db7a7770e473c9f136d0abd945f1c08f9e02d291
Rakshasetty <rakshasetty004@gmail.com>
2023-02-17 20:24:36 +0530

    complete component part

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from Census.pipeline.training_pipeline import start_training_pipeline
-
+from Census.pipeline.batch_prediction import start_batch_prediction 
 file_path="/config/workspace/adult.csv"
 print(__name__)
 if __name__=="__main__":
      try:
-          start_training_pipeline()
+    #      start_training_pipeline()
+          output_file = start_batch_prediction(input_file_path=file_path)
+          print(output_file)
      except Exception as e:
           print(e)
